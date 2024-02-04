35c712d5fd2f8a2521da5f981093b5030882008a
Gage <gage.bonner@gmail.com>
2024-02-04 11:58:37 -0500

    typo

--- src/SargassumColors.jl
+++ src/SargassumColors.jl
@@ -2,10 +2,10 @@ module SargassumColors
 
 using Colors, ColorSchemes
 
-include(join(@__DIR__, "eureka.jl"))
+include(joinpath(@__DIR__, "eureka.jl"))
 export EUREKA
 
-include(join(@__DIR__, "shadden.jl"))
+include(joinpath(@__DIR__, "shadden.jl"))
 export SHADDEN
 
 end
